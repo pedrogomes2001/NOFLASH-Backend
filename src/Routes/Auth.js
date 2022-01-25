e0ef220ba2a09b64ed631f13751943d26a688744
pedrogomes2001 <pmlg2001@gmail.com>
2022-01-25 16:06:25 +0000

    routes

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -5,12 +5,6 @@ import { json } from "body-parser";
 const router = Router();
 const Controller = new Auth();
 
-router.post('/api/signup', json(), (request, response)=>{
-    Controller.signup(request, response)
-});
 
-router.post('/api/signin',json(), (request, response)=>{
-    Controller.signin(request, response)
-});
 
 export default router
