e319f11b4a8bdfb3bff978d8b21f197cf8a96d33
pedrogomes2001 <pmlg2001@gmail.com>
2022-01-25 16:37:24 +0000

    Authenticate

--- src/Middleware/Authenticate.js
+++ src/Middleware/Authenticate.js
@@ -1,13 +1,7 @@
 import { verify } from "jsonwebtoken";
 
-export const Authenticate = (request, response, next)=>{
+export const Authenticate = (request, response, next) => {
     const token = request.headers['x-auth-token'];
 
-    try {
-        const validatedToken = verify(token, process.env.cookie_secret)
-        request.token = validatedToken;
-        next()
-    } catch (error) {
-        return response.status(403).json({error:'Sign in or create account to continue'})
-    }
+
 }
